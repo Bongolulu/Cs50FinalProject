b69d26b4020390350debdc557b1cff0a365b927d
unimatrix27 <sebastian.stuecker@gmail.com>
2024-04-01 14:54:25 +0200

    loginformular wie registerformular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl ('', [Validators.required], []),
+    password: new FormControl ('', [Validators.required], []),
+  });
 
+  constructor(){
+    this.loginForm.valueChanges.subscribe(console.log)
+  }
+  Login(){
+  }
 }
