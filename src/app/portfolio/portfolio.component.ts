92d3314392b7ecaa2be8244ca60d251812e7e5ac
unimatrix27 <sebastian.stuecker@gmail.com>
2024-04-03 14:56:45 +0200

    portfolio wird angezeigt

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -9,16 +9,18 @@ import { ApiService } from '../api.service';
   styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent implements OnInit {
-  portfolio = [];
+  portfolio: any = []; // TODO später any durch richtige typen ersetzen um fehler zu vermeiden
   bargeld: number = 5;
   gesamtwert: number = 4;
 
   constructor(private apiService: ApiService) {}
   ngOnInit(): void {
-    this.apiService.Portfolio().subscribe({ //Ich rufe die Funktion im api.service.ts ab
+    this.apiService.Portfolio().subscribe({
+      //Ich rufe die Funktion im api.service.ts ab
       next: (antwort) => {
-        console.log(antwort);
-        this.portfolio = antwort;
+        // Antwort auf Symbol, Anzahl,... (portfolio)
+        console.log(antwort.bestand);
+        this.portfolio = antwort.bestand;
       },
       error: (fehler) => {
         console.log(fehler);
